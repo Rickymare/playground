3630bd5748d512a0561afef5ec5c0b4d9fff3a12
Rickymare <rickypwnage@gmail.com>
2017-04-03 14:02:30 -0700

    initial commit

---
+++ script.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Create a folder
+
+mkdir ~/projects/playground/testdir
+
+# Create a shell script
+
+touch ~/projects/playground/testdir/test.sh
+ 
+# Change permissions so the script can be executed
+
+chmod 755 ~/projects/playground/testdir/test.sh
+
