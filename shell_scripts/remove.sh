971f5889b44eceee041e262605c208a87a067ca1
Rickymare <rickypwnage@gmail.com>
2017-04-03 18:00:22 -0700

    added a remove script to clean up

---
+++ shell_scripts/remove.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Changing in to the directory to delete files I created
+
+cd ~/projects/playground/shell_scripts
+
+# Removing folder
+
+rm -r testdir/
+
+# Display contents of folder to verify the folder was removed
+
+ls -al
--- shell_scripts/script.sh
+++ shell_scripts/script.sh
@@ -1,14 +1,29 @@
 #!/bin/sh
 
+# Changing directory to the directory the script will function in
+
+cd ~/projects/playground/shell_scripts
+
 # Create a folder
 
-mkdir ~/projects/playground/shell_scripts/testdir
+mkdir testdir
+
+# Change in to directory that was created
+
+cd testdir/
 
 # Create a shell script
 
-touch ~/projects/playground/shell_scripts/testdir/test.sh
+touch test.sh
+
+# Adding text to file
+
+echo "#!/bin/sh" >> test.sh
  
 # Change permissions so the script can be executed
 
-chmod 755 ~/projects/playground/shell_scripts/testdir/test.sh
+chmod 755 test.sh
 
+# Display the script
+
+cat test.sh
