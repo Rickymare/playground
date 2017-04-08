45890ef05120253053f1146a4ac1d775ff647321
Rickymare <rickypwnage@gmail.com>
2017-04-07 21:48:25 -0700

    Added apache2, mysql-server, and php files to be removed

--- shell_scripts/remove.sh
+++ shell_scripts/remove.sh
@@ -1,13 +1,16 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-# Changing in to the directory to delete files I created
+# Run this script if data.sh or web.sh was unsuccessful. This script will remove the files that were added so we can try again with a fresh install.
 
-cd ~/projects/playground/shell_scripts
+# Remove apache2
+sudo apt -y remove apache2
 
-# Removing folder
+# Remove MySql-server
+sudo apt -y remove mysql-server
 
-rm -r testdir/
+# Remove PHP and its dependencies
+sudo apt -y remove php libapache2-mod-php php-mcrypt php-mysql
 
-# Display contents of folder to verify the folder was removed
+# Removing dependencies
+sudo apt -y autoremove
 
-ls -al
