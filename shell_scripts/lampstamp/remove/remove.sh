b1131fe021ad7702530a007d367648d1ac2c2ab9
Rickymare <rickypwnage@gmail.com>
2017-04-11 17:56:06 -0700

    added -y to automatically remove dependencies

--- shell_scripts/lampstamp/remove/remove.sh
+++ shell_scripts/lampstamp/remove/remove.sh
@@ -3,8 +3,8 @@
 # Run this script if data.sh or web.sh was unsuccessful. This script will remove the files that were added so we can try again with a fresh install.
 
 # remove apache2
-sudo apt remove apache2
+sudo apt -y remove apache2
 
 # Remove MySql-server
-sudo apt remove mysql-server
+sudo apt -y remove mysql-server
 
