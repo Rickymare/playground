f8e2e527692b3deaef3a027d304989fd23a96e29
rickymare <rickypwnage@gmail.com>
2017-04-11 19:01:18 -0700

    edited the dir.conf to prioritize index.php

--- shell_scripts/lampstamp/web/web.sh
+++ shell_scripts/lampstamp/web/web.sh
@@ -15,5 +15,11 @@ sudo apt -y install apache2
 # Adding ServerName and IP
 echo "ServerName 10.154.31.45" >> /etc/apache2/apache2.conf
 
+# Edit dir.conf and prioritize index.php
+echo "
+<IfModule mod_dir.c>
+     DirectoryIndex index.php index.html index.cgi index.pl index.xhtml index.htm
+</IfModule>" >> /etc/apache2/mods-enabled/dir.conf
+
 # Restarting apache2
 sudo systemctl restart apache2
