d129ffa3b5ec4a803892b264a1bfda5751d46b31
rickymare <rickypwnage@gmail.com>
2017-04-13 16:31:06 -0700

    added -y to auto upgrade

--- shell_scripts/lampstamp/data/data.sh
+++ shell_scripts/lampstamp/data/data.sh
@@ -7,14 +7,14 @@ set -u
 apt update -y
 
 # Upgrade
-sudo apt upgrade
+sudo apt upgrade -y
  
 # Pre-seed debconf with MySQL server answers
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password password'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password password'
  
 # Install mySql server
-sudo apt -y install mysql-server
+sudo apt install mysql-server -y
  
 # Edit the bind address with ip address
 sed -i 's/bind-address/bind-address = 10.154.31.45/' /etc/mysql/mysql.conf.d/mysqld.cnf
