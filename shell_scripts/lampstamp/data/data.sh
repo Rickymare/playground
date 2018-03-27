9b802f9cf133acfd8da142c5c1c80a4ee72ab6de
Ricky Sanchez <Rickypwnage@gmail.com>
2018-03-27 13:21:23 -0700

    Corrected casing on Mysql to mysql

--- shell_scripts/lampstamp/data/data.sh
+++ shell_scripts/lampstamp/data/data.sh
@@ -20,7 +20,7 @@ sudo apt install mysql-server -y
 sudo sed -i 's/bind-address/bind-address = localhost/' /etc/mysql/mysql.conf.d/mysqld.cnf
 
 # Create database. user, password, and grant privileges in MySQL
-Mysql -uroot -ppassword -e "CREATE DATABASE wordpress;"
+mysql -uroot -ppassword -e "CREATE DATABASE wordpress;"
 mysql -uroot -ppassword -e "CREATE USER 'wordpressuser'@'localhost' IDENTIFIED BY 'password';"
 mysql -uroot -ppassword -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpressuser'@'localhost';"
 mysql -uroot -ppassword -e "FLUSH PRIVILEGES;"
