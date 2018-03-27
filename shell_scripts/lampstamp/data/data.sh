048bfb7f1683e54ab0b7ad8035c2140de1749bf1
Ricky Sanchez <Rickypwnage@gmail.com>
2018-03-27 13:18:19 -0700

    Created user and restarted MySQL

--- shell_scripts/lampstamp/data/data.sh
+++ shell_scripts/lampstamp/data/data.sh
@@ -18,3 +18,15 @@ sudo apt install mysql-server -y
  
 # Edit the bind address with ip address
 sudo sed -i 's/bind-address/bind-address = localhost/' /etc/mysql/mysql.conf.d/mysqld.cnf
+
+# Create database. user, password, and grant privileges in MySQL
+Mysql -uroot -ppassword -e "CREATE DATABASE wordpress;"
+mysql -uroot -ppassword -e "CREATE USER 'wordpressuser'@'localhost' IDENTIFIED BY 'password';"
+mysql -uroot -ppassword -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpressuser'@'localhost';"
+mysql -uroot -ppassword -e "FLUSH PRIVILEGES;"
+
+# Restart MySQL
+sudo systemctl restart mysql.service
+ 
+# End of script message
+echo "completed successfully!"
