f7e32bf9163368811d666d03ad8ccf96a27eeb4d
Ricky Sanchez <Rickypwnage@gmail.com>
2018-03-28 16:04:18 -0700

    Update web.sh

--- shell_scripts/lampstamp/web/web.sh
+++ shell_scripts/lampstamp/web/web.sh
@@ -1,61 +1,58 @@
-!/usr/bin/env bash
+#!/usr/bin/env bash
 
 set -e
 set -u
 
-# Update APT cache
-sudo apt update
+# Update the APT cache
+sudo apt update -y
 
-# Upgrade
+# Upgrade the apt repos to ensure we install the LTS
 sudo apt upgrade -y
 
 # Install Apache2
 sudo apt install apache2 -y
 
-# Install PHP and its dependencies
+# Install PHP and the dependencies
 sudo apt install php7.0 libapache2-mod-php7.0 php7.0-mcrypt php7.0-mysql php7.0-gd php-ssh2 -y
 
-# Install MySQL
+# Install MySQL client
 sudo apt install mysql-client -y
 
 # Adding ServerName and IP
 echo "ServerName localhost" | sudo tee --append /etc/apache2/apache2.conf
 
-# Edit dir.conf and prioritize index.php
+# Edit the dir.conf and prioritize index.php
 echo "
 <IfModule mod_dir.c>
      DirectoryIndex index.php index.html index.cgi index.pl index.xhtml index.htm
 </IfModule>" | sudo tee /etc/apache2/mods-enabled/dir.conf
 
-# Download Wordpress
-wget http://wordpress.org/latest.tar.gz -O ~/latest.tar.gz
+# Download Wordpress into /tmp directory
+wget http://wordpress.org/latest.tar.gz -O /tmp/latest.tar.gz
 
-# Unzip WordPress file
-sudo tar xzvf ~/latest.tar.gz -C /
+# Unzip the WordPress
+sudo tar xzvf /tmp/latest.tar.gz -C /
 
-# Copy wp-config-sample.php to wp-config.php
+# Copy the wp-config-sample.php to wp-config-php
 sudo cp /wordpress/wp-config-sample.php /wordpress/wp-config.php
 
-# Edit the wp-config file to include user, database, and password
-sudo sed -i "/DB_NAME/c\define('DB_NAME', 'wordpress');/" /wordpress/wp-config.php
+# Edit the wp-config file to include databse, user, password, and data IP
+sudo sed -i "/DB_NAME/c\define('DB_NAME', 'wordpress');" /wordpress/wp-config.php
 
 sudo sed -i "/DB_USER/c\define('DB_USER', 'wordpressuser');" /wordpress/wp-config.php
 
-sudo sed -i "/DB_PASSWORD/c\define('DB_PASSWORD', 'password');/" /wordpress/wp-config.php
+sudo sed -i "/DB_PASSWORD/c\define('DB_PASSWORD', 'password');" /wordpress/wp-config.php
 
 sudo sed -i "/DB_HOST/c\define('DB_HOST', 'localhost');" /wordpress/wp-config.php
 
-# Copy Wordpress folder into document root
+# Copy WordPress folder into document root
 sudo rsync -avP /wordpress/ /var/www/html/
 
-# Creating uploads folder
-sudo mkdir -p /var/www/html/wp-content/uploads
+# Creating an uploads folder
+sudo mkdir /var/www/html/wp-content/uploads
 
-# Giving permissions to user and group
+# Giving permissions to user and group(apache2) 
 sudo chown -R ubuntu:www-data /var/www/html
 
 # Restarting apache2
 sudo systemctl restart apache2
-
-# end of script
-echo "successfully completed"
