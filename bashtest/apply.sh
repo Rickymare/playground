4f92ee7c32da7f7b7dd615b18356ae2d3c7a60cc
rickymare <rickypwnage@gmail.com>
2018-03-29 14:46:52 -0700

    Moved user input up

--- bashtest/apply.sh
+++ bashtest/apply.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# Ask user is shell script is running on same server or different servers
+echo "Is this running on the same server or different servers?"
+
+read ans
+
+echo "this is running on" $ans "servers"
+
 # I am about to run a.sh
 echo "I am about to run a.sh"
 ./a.sh
@@ -7,10 +14,3 @@ echo "I am about to run a.sh"
 # I am about to run b.sh
 echo "I am about to run b.sh"
 ./b.sh
-
-# Ask user is shell script is running on same server or different servers
-echo "Is this running on the same server or different servers?"
-
-read ans
-
-echo "this is running on" $ans "servers"
