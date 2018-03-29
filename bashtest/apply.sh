dd39af3845fa10aeee315f09ed392931e432e3dd
rickymare <rickypwnage@gmail.com>
2018-03-29 16:00:04 -0700

    Added if statement

--- bashtest/apply.sh
+++ bashtest/apply.sh
@@ -5,7 +5,17 @@ echo "Is this running on the same server or different servers?"
 
 read ans
 
-echo "This is running on $ans servers"
+if [ $ans = "different" ]
+then
+		echo "What is the name of server A?"
+		
+		read ans2
+
+		echo "What is the name of server B"?
+
+		read ans3
+
+fi
 
 # I am about to run a.sh
 echo "I am about to run a.sh"
