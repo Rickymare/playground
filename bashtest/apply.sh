5f1e6e1cf362fa7a9cec188d0861d459ddce2aba
rickymare <rickypwnage@gmail.com>
2018-03-29 14:44:28 -0700

    Added user input

--- bashtest/apply.sh
+++ bashtest/apply.sh
@@ -7,3 +7,10 @@ echo "I am about to run a.sh"
 # I am about to run b.sh
 echo "I am about to run b.sh"
 ./b.sh
+
+# Ask user is shell script is running on same server or different servers
+echo "Is this running on the same server or different servers?"
+
+read ans
+
+echo "this is running on" $ans "servers"
