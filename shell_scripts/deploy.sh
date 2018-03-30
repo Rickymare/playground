8203a8dcecfc782219ea9e09dcf5bb5b85969e53
Ricky Sanchez <rickypwnage@gmail.com>
2018-03-30 11:37:07 -0700

    Replaced web with tasks

--- shell_scripts/deploy.sh
+++ shell_scripts/deploy.sh
@@ -5,19 +5,20 @@ set -u
 
 # Ask user would they like to do?
 echo "What would you like to do?"
-echo "1. Deploy web server"
-echo "2. Deploy data server"
+echo "1. Deploy web task"
+echo "2. Deploy data task"
 echo "3. Deploy both"
 
 read server
 
-if [ $server = "1" ]
+if [ $server = "web" ]
 then 
-		echo "Executing web server"
-elif [ $server = "2" ]
+		echo "Executing web tasks"
+elif [ $server = "data" ]
 then
-		echo "Executing data server"
-elif [ $server = "3" ]
+		echo "Executing data tasks"
+elif [ $server = "both" ]
 then
-		echo "Executing web and data servers"
+		echo "Executing web tasks"
+		echo "Executing data tasks"
 fi
