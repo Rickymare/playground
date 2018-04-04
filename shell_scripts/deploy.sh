8abbbce1dc085195c9ffb3795136ff17f18e1d09
Ricky Sanchez <rickypwnage@gmail.com>
2018-04-03 17:16:26 -0700

    Added -help and allowed arguments

--- shell_scripts/deploy.sh
+++ shell_scripts/deploy.sh
@@ -3,6 +3,7 @@
 set -e
 set -u
 
+# Functions
 web_tasks () {
 	echo "Executing web tasks"
 }
@@ -11,24 +12,41 @@ data_tasks () {
 	echo "Executing data tasks"
 }
 
-# Ask user what would they like to do?
-echo "What would you like to do?"
-echo "1. Deploy web task"
-echo "2. Deploy data task"
-echo "3. Deploy both"
 
-read server
-
-if [ $server = "web" ] || [ $server = "1" ]
+# Check to see if there are any arguments
+if [ $# -ge 1 ]
 then
-    web_tasks
-elif [ $server = "data" ] || [ $server = "2" ]
-then
-	data_tasks
-elif [ $server = "both" ] || [ $server = "3" ]
-then
-	data_tasks
-	web_tasks
+    tasks=$1
+    if [ $tasks = "help" ]
+    then
+        echo "Server options: web. data, or both"
+        exit 0
+    elif [ $tasks = "web" ]
+    then
+        web_tasks
+    elif [ $tasks = "data" ]
+    then
+        data_tasks
+    elif [ $tasks = "both" ]
+    then
+        web_tasks
+        data_tasks
+    fi
 else
-	echo "Choose between 'web' 'data' or 'both'"
+    echo "What would you like to launch?"
+    echo "1. Deploy web task"
+    echo "2. Deploy data task"
+    echo "3. Deploy both tasks"
+    read server
+    if [ $server = "web" ] || [ $server = "1" ]
+    then
+        web_tasks
+    elif [ $server = "data" ] || [ $server = "2" ]
+    then
+        data_tasks
+    elif [ $server = "both" ] || [ $server = "3" ]
+    then
+        web_tasks
+        data_tasks
+    fi
 fi
