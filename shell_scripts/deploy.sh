07fc998a7830b6a9ea685a2986683b1cf35f38c5
Ricky Sanchez <rickypwnage@gmail.com>
2018-04-03 20:20:41 -0700

    Removed the extra if and reused variable

--- shell_scripts/deploy.sh
+++ shell_scripts/deploy.sh
@@ -17,36 +17,26 @@ data_tasks () {
 if [ $# -ge 1 ]
 then
     tasks=$1
-    if [ $tasks = "help" ]
-    then
-        echo "Server options: web. data, or both"
-        exit 0
-    elif [ $tasks = "web" ]
-    then
-        web_tasks
-    elif [ $tasks = "data" ]
-    then
-        data_tasks
-    elif [ $tasks = "both" ]
-    then
-        web_tasks
-        data_tasks
-    fi
 else
     echo "What would you like to launch?"
     echo "1. Deploy web task"
     echo "2. Deploy data task"
-    echo "3. Deploy both tasks"
-    read server
-    if [ $server = "web" ] || [ $server = "1" ]
-    then
-        web_tasks
-    elif [ $server = "data" ] || [ $server = "2" ]
-    then
-        data_tasks
-    elif [ $server = "both" ] || [ $server = "3" ]
-    then
-        web_tasks
-        data_tasks
-    fi
+    echo "3. Deploy both"
+    read tasks
+fi
+
+if [ $tasks = "web" ] || [ $tasks = "1" ]
+then
+    web_tasks
+elif [ $tasks = "data" ] || [ $tasks = "2" ]
+then
+    data_tasks
+elif [ $tasks = "both" ] || [ $tasks = "3" ]
+then
+    web_tasks
+    data_tasks
+elif [ $tasks = "help" ]
+then
+    echo "Server options: web, data, or both"
+    exit 0
 fi
